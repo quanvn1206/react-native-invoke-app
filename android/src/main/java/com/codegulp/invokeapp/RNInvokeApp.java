1b4f592b1bd5f04d49737eb24e4fdee4514e98de
Roman Suvorov <roma232789@gmail.com>
2020-06-17 20:53:35 +0300

    1.2.8

--- android/src/main/java/com/codegulp/invokeapp/RNInvokeApp.java
+++ android/src/main/java/com/codegulp/invokeapp/RNInvokeApp.java
@@ -37,6 +37,7 @@ public class RNInvokeApp extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void invokeApp(ReadableMap params) {
+        Log.d('HAS_KEY_PARAM', params.hasKey("data"))
         ReadableMap data = params.hasKey("data") ? params.getMap("data") : null;
 
         for (String key: bundle.keySet()) {
